c33d221c677a67b143e3579ddd7a196e5bf32de9
Myrna Alcaide <malcaide@twilio.com>
2023-09-17 12:02:37 -0700

    moar modifications for posts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
 import sqlite3
+from flask import Flask, request, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
@@ -20,21 +21,18 @@ in_memory_datastore = {
            "contribution": "iterators, abstract data types, generics, checked exceptions"},
 }
 
+# helper functions
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/programming_languages', methods=[GET_METHOD, POST_METHOD])
-def programming_languages_route():
-    if request.method == GET_METHOD:
-        return list_programming_languages()
-    elif request.method == POST_METHOD:
-        return create_programming_language(request.get_json(force=True))
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    return post
 
 def list_programming_languages():
     before_year = request.args.get('before_year') or '30000'
@@ -50,6 +48,26 @@ def create_programming_language(new_lang):
     in_memory_datastore[language_name] = new_lang
     return new_lang
 
+# routes
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template("index.html", posts=posts)
+
+@app.route('/programming_languages', methods=[GET_METHOD, POST_METHOD])
+def programming_languages_route():
+    if request.method == GET_METHOD:
+        return list_programming_languages()
+    elif request.method == POST_METHOD:
+        return create_programming_language(request.get_json(force=True))
+
 @app.route('/programming_languages/<programming_language_name>')
 def get_programming_language_name(programming_language_name):
     return in_memory_datastore[programming_language_name]
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
